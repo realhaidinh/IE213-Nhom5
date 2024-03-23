01d8cf8319e283dacd830a80281bec4279739e90
Hieu192 <126752588+Hieu192@users.noreply.github.com>
2024-03-23 21:14:40 +0700

    comment

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// import Bt4 from './bt4.js';
+import Bt1 from './bt1.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Bt1 />
   </React.StrictMode>
 );
 
